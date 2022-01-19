117e32997a98a60cfc89d9edfba930e793e76749
☆ RAKA ☆™︻®╤──────═◍➤ <95731529+Garangan-Z@users.noreply.github.com>
2022-01-20 01:36:02 +0700

    Update RAKA.py

--- RAKA.py
+++ RAKA.py
@@ -222,7 +222,7 @@ def publik():
 			id.append(uid+"<=>"+nama)
 	except KeyError:
 		exit("\033[1;93m⋆✥⋆➣\033[1;97m Account Friend List Is Not Public")
-	print("\n\033[1;96m[\033[1;93m•\033[1;96m] \033[1;92m⋆✥⋆➣\033[1;97m Total Id  : \033[0;92m%s\033[0;97m"%(len(id))) 
+	print("\n\033[1;96m[\033[1;93m•\033[1;96m] \033[1;92m⋆✥⋆➣\033[1;97m Total Id   : \033[0;92m%s\033[0;97m"%(len(id))) 
 
 def follower():
 	global token
@@ -262,7 +262,7 @@ def massal():
 	print("[\033[1;96m[\033[1;93m•\033[1;96m] \033[1;92m⋆✥⋆➣\033[1;97m Total id    : \033[0;92m%s\033[0;96m"%(len(id)))
 
 def method():
-	print("\n\033[1;96m[\033[1;93m•\033[1;96m] \033[1;92m⋆✥⋆➣\033[1;97m Choose Crack Methode")
+	print("\033[1;96m[\033[1;93m•\033[1;96m] \033[1;92m⋆✥⋆➣\033[1;97m Choose Crack Methode\n")
 	print("\033[1;96m[\033[1;93m1\033[1;96m] \033[1;92m⋆✥⋆➣ \033[1;97mB-API\033[1;97m [ \033[1;95mFast \033[1;97m]")
 	print("\033[1;96m[\033[1;93m2\033[1;96m] \033[1;92m⋆✥⋆➣ \033[1;97mM-Basic\033[1;97m [ \033[1;95mFast \033[1;97m]")
 	print("\033[1;96m[\033[1;93m3\033[1;96m] \033[1;92m⋆✥⋆➣ \033[1;97mFree Facebook\033[1;97m [ \033[1;95mNormal \033[1;97m]\n")
