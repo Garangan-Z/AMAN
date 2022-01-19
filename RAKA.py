3874e99df09f85997f61e540087543c553d19bfc
☆ RAKA ☆™︻®╤──────═◍➤ <95731529+Garangan-Z@users.noreply.github.com>
2022-01-20 01:31:04 +0700

    Update RAKA.py

--- RAKA.py
+++ RAKA.py
@@ -222,7 +222,7 @@ def publik():
 			id.append(uid+"<=>"+nama)
 	except KeyError:
 		exit("\033[1;93m⋆✥⋆➣\033[1;97m Account Friend List Is Not Public")
-	print("[\033[1;96m[\033[1;93m•\033[1;96m] \033[1;92m⋆✥⋆➣\033[1;97m Total Id        : \033[0;91m%s\033[0;97m"%(len(id))) 
+	print("\n\033[1;96m[\033[1;93m•\033[1;96m] \033[1;92m⋆✥⋆➣\033[1;97m Total Id  : \033[0;91m%s\033[0;97m"%(len(id))) 
 
 def follower():
 	global token
